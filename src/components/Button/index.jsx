9b2d6d67e88bcf865c827a79a109382affb3635b
Luis Farfan <luise.prado99@hotmail.com>
2022-10-13 21:06:58 -0600

    fix: Button with href fixed

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import './Button.scss'
 
 const Button = ({ color, text, className = '', onClick, type = 'button', href = '' }) => {
-  if (href !== '') return <a target="_blank" href={href} className={`btn ${color} ${className}`} onClick={onClick}>{text}</a>
+  if (href !== '') {
+    return (
+      <button className={`btn ${color} ${className}`} onClick={onClick}>
+        <a target="_blank" href={href}>{text}</a>
+      </button>
+    )
+  }
 
   return (
     <button type={type} className={`btn ${color} ${className}`} onClick={onClick}>{text}</button>
