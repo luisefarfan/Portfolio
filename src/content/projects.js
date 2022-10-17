aaf80b9a96f1bce747034891d7fa5a8d4aa86a37
Luis Enrique Farfán Prado <luis.farfan@telus.com>
2022-10-17 11:52:03 -0600

    feat: projects content added

---
+++ src/content/projects.js
@@ -0,0 +1,41 @@
+const projectsContent = {
+  en: {
+    title: 'Projects',
+    text: 'asdf asdf asdf',
+    projects: [
+      {
+        title: 'Koleyo',
+        subtitle: 'Education tool',
+        description: '',
+        url: 'https://www.koleyo.com'
+      },
+      {
+        title: 'Priscilla Bianchi',
+        subtitle: 'Artist\'s landing page and e-commerce',
+        description: 'https://www.priscillabianchi.net',
+        url: ''
+      },
+      {
+        title: 'Carolina Mendez',
+        subtitle: 'Wedding Planner fully managed landing page',
+        description: 'https://www.carolinamendez.com.gt',
+        url: ''
+      },
+      {
+        title: 'Servpresa',
+        subtitle: 'Industrial Security company\'s landing page',
+        description: 'https://www.servpresa.com.gt',
+        url: ''
+      }
+    ]
+  },
+  es: {
+    title: 'Proyectos',
+    text: 'asdf asdf asdf',
+    projects: [
+
+    ]
+  }
+}
+
+export default projectsContent
