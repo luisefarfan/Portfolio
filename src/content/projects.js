c473b4e9677dc324d24d789e636ad3d5aef93700
Luis Farfan <luise.prado99@hotmail.com>
2022-10-20 19:39:02 -0600

    feat: projects content added

--- src/content/projects.js
+++ src/content/projects.js
@@ -3,6 +3,7 @@ import project2Img from '../assets/pages/Projects/priscilla.png'
 import project3Img from '../assets/pages/Projects/carolina.png'
 import project4Img from '../assets/pages/Projects/servpresa.png'
 import project5Img from '../assets/pages/Projects/squadra.png'
+import portfolioV1Img from '../assets/pages/Projects/portfolioV1.png'
 
 const projectsContent = {
   en: {
@@ -13,38 +14,45 @@ const projectsContent = {
     projects: [
       {
         title: 'Koleyo',
-        subtitle: 'Education tool',
-        description: '',
+        subtitle: '',
+        description: 'Application created with Symfony, Twig, Javascript, CSS and MySQL that aims to make it easier for independent teachers and small educational institutions to manage and control their students and courses.',
         url: 'https://www.koleyo.com',
         img: project1Img
       },
       {
         title: 'Priscilla Bianchi',
-        subtitle: 'Artist\'s landing page and e-commerce',
-        description: '',
+        subtitle: '',
+        description: 'E-commerce and Landing page for a Guatemalan artist. Made with Symfony, Twig, Javascript, CSS and MySQL. It is a website created specifically to the artist, which is also fully manageable. It has the ability to add or remove content in markdown format.',
         url: 'https://www.priscillabianchi.net',
         img: project2Img
       },
       {
         title: 'Carolina Mendez',
-        subtitle: 'Wedding Planner fully managed landing page',
-        description: '',
+        subtitle: '',
+        description: 'Fully manageable landing page created to showcase the work of Carolina Méndez, Guatemalan Wedding Planner.',
         url: 'https://www.carolinamendez.com.gt',
         img: project3Img
       },
       {
         title: 'Servpresa',
-        subtitle: 'Industrial Security company\'s landing page',
-        description: '',
+        subtitle: '',
+        description: 'Landing page for an industrial security and outsourcing company. It has the ability to receive CVs, which are sent to the corresponding people by mail.',
         url: 'https://www.servpresa.com.gt',
         img: project4Img
       },
       {
         title: 'Squadra',
-        subtitle: 'HHRR Company\'s landing page',
-        description: '',
+        subtitle: '',
+        description: 'Landing page created with HTML, CSS and Javascript for a human resources company.',
         url: 'https://www.squadra.com.gt',
         img: project5Img
+      },
+      {
+        title: 'Portafolio V1',
+        subtitle: '',
+        description: 'First version of my portfolio, created with React and TailwindCSS.',
+        url: 'https://luis-farfan.vercel.app',
+        img: portfolioV1Img
       }
     ]
   },
@@ -56,38 +64,45 @@ const projectsContent = {
     projects: [
       {
         title: 'Koleyo',
-        subtitle: 'Education tool',
-        description: '',
+        subtitle: '',
+        description: 'Aplicación creada con Symfony, Twig, Javascript, CSS y MySQL que tiene el objetivo de facilitarles la administración y control de sus estudiantes y cursos a maestros independientes e instituciones educativas pequeñas.',
         url: 'https://www.koleyo.com',
         img: project1Img
       },
       {
         title: 'Priscilla Bianchi',
-        subtitle: 'Artist\'s landing page and e-commerce',
-        description: '',
+        subtitle: '',
+        description: 'E-commerce y Landing page para artista guatemalteca. Realizada con Symfony, Twig, Javascript, CSS y MySQL. Es una página web hecha a la medida de la artista, que a la vez es totalmente administrable. Cuenta con la capacidad de agregar o eliminar contenido en formato de markdown.',
         url: 'https://www.priscillabianchi.net',
         img: project2Img
       },
       {
         title: 'Carolina Mendez',
-        subtitle: 'Wedding Planner fully managed landing page',
-        description: '',
+        subtitle: '',
+        description: 'Landing page totalmente administrable creada para mostrar el trabajo de Carolina Méndez, Wedding Planner guatemalteca.',
         url: 'https://www.carolinamendez.com.gt',
         img: project3Img
       },
       {
         title: 'Servpresa',
-        subtitle: 'Industrial Security company\'s landing page',
-        description: '',
+        subtitle: '',
+        description: 'Landing page para empresa de seguridad industrial y outsourcing. Cuenta con la capacidad de recibir CVs, los cuales son enviados a las personas correspondientes por medio de correo.',
         url: 'https://www.servpresa.com.gt',
         img: project4Img
       },
       {
         title: 'Squadra',
-        subtitle: 'HHRR Company\'s landing page',
-        description: '',
+        subtitle: '',
+        description: 'Landing page creada con HTML, CSS y Javascript para empresa de recursos humanos.',
         url: 'https://www.squadra.com.gt',
         img: project5Img
+      },
+      {
+        title: 'Portafolio V1',
+        subtitle: '',
+        description: 'Primera versión de mi portafolio, creado con React y TailwindCSS.',
+        url: 'https://luis-farfan.vercel.app',
+        img: portfolioV1Img
       }
     ]
   }
