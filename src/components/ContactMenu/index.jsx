6bbd5341110ac776c25b8eab3ddb49c61cf2c1cb
Luis Farfan <luise.prado99@hotmail.com>
2022-10-20 19:21:27 -0600

    feat: contact menu hover icon change

--- src/components/ContactMenu/index.jsx
+++ src/components/ContactMenu/index.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NormalImage } from '../Image'
 import phoneImg from '../../assets/components/ContactMenu/phone.svg'
+import phoneHoverImg from '../../assets/components/ContactMenu/phoneHover.svg'
 import emailImg from '../../assets/components/ContactMenu/email.svg'
+import emailHoverImg from '../../assets/components/ContactMenu/emailHover.svg'
 import linkedinImg from '../../assets/components/ContactMenu/linkedin.svg'
+import linkedinHoverImg from '../../assets/components/ContactMenu/linkedinHover.svg'
 import githubImg from '../../assets/components/ContactMenu/github.svg'
+import githubHoverImg from '../../assets/components/ContactMenu/githubHover.svg'
 import verticalPoints from '../../assets/components/ContactMenu/verticalPoints.svg'
 import content from '../../content/contactMenu'
 import './ContactMenu.scss'
 
 const ContactMenu = ({ showContactMenu, fromMobile }) => {
   const { phone, email, linkedin, github } = content
+  const [hoveredContact, setHoveredContact] = useState('')
+
   return (
     <div className={`contact-menu ${showContactMenu && 'active'} ${fromMobile && 'mobile'}`}>
       <NormalImage src={verticalPoints} className="mb-5 xl:block hidden" />
-      <a href={`tel:${phone}`} className="contact-item">
-        <NormalImage src={phoneImg} />
+      <a onMouseEnter={() => setHoveredContact('phone')} onMouseLeave={() => setHoveredContact('')} href={`tel:${phone}`} className="contact-item">
+        <NormalImage src={hoveredContact === 'phone' ? phoneHoverImg : phoneImg} />
       </a>
-      <a href={`mailto:${email}`} className="contact-item">
-        <NormalImage src={emailImg} />
+      <a onMouseEnter={() => setHoveredContact('email')} onMouseLeave={() => setHoveredContact('')} href={`mailto:${email}`} className="contact-item">
+        <NormalImage src={hoveredContact === 'email' ? emailHoverImg : emailImg} />
       </a>
-      <a href={linkedin} className="contact-item" target="_blank">
-        <NormalImage src={linkedinImg} />
+      <a onMouseEnter={() => setHoveredContact('linkedin')} onMouseLeave={() => setHoveredContact('')} href={linkedin} className="contact-item" target="_blank">
+        <NormalImage src={hoveredContact === 'linkedin' ? linkedinHoverImg : linkedinImg} />
       </a>
-      <a href={github} className="contact-item" target="_blank">
-        <NormalImage src={githubImg} />
+      <a onMouseEnter={() => setHoveredContact('github')} onMouseLeave={() => setHoveredContact('')} href={github} className="contact-item" target="_blank">
+        <NormalImage src={hoveredContact === 'github' ? githubHoverImg : githubImg} />
       </a>
     </div>
   )
