f5088cb20a839bb286eb9b82cb5ca4f97d073166
Luis Farfan <luise.prado99@hotmail.com>
2022-10-20 22:27:08 -0600

    fix: click the anchor tag when the button is clicked

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Button.scss'
 
-const Button = ({ color, text, className = '', onClick, type = 'button', href = '' }) => {
+const Button = ({ color, text, className = '', onClick = () => { }, type = 'button', href = '' }) => {
+  const ref = useRef()
+
+  const handleAnchorClick = (e) => {
+    ref.current.click()
+    onClick(e)
+  }
+
   if (href !== '') {
     return (
-      <button type={type} className={`btn ${color} ${className}`} onClick={onClick}>
-        <a target="_blank" href={href}>{text}</a>
+      <button type={type} className={`btn ${color} ${className}`} onClick={handleAnchorClick}>
+        <a ref={ref} target="_blank" href={href}>{text}</a>
       </button>
     )
   }
